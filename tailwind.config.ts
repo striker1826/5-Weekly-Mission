12f4377da20a97d6b43d470a51ff9695829de474
striker1826 <alstjq123579@daum.net>
2024-05-08 19:59:55 +0900

    프로젝트 초기 셋팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./widgets/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -12,6 +13,17 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        Primary: "#6D6AFE",
+        Red: "#FF5B56",
+        Black: "#111322",
+        White: "#FFFFFF",
+        Gray1: "#3E3E43",
+        Gray2: "9FA6B2",
+        Gray3: "#CCD5E3",
+        Gray4: "#E7EFFB",
+        Gray5: "#F0F6FF",
       },
     },
   },
