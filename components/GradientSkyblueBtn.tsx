861209dc64d790ee39ca1be8de9b7538e8c3a849
striker1826 <alstjq123579@daum.net>
2024-05-08 20:49:43 +0900

    Main Section 구현

--- components/GradientSkyblueBtn.tsx
+++ components/GradientSkyblueBtn.tsx
@@ -8,7 +8,7 @@ interface PropsType {
 const GradientSkyblueBtn: React.FC<PropsType> = ({ width, text }) => {
   return (
     <button
-      className={`w-[${width}] rounded-[8px] bg-gradient-to-r from-purple-600 to-sky-400 px-[20px] py-[16px] text-[18px] font-[600] text-neutral-100`}
+      className={`w-[${width}px] rounded-[8px] bg-gradient-to-r from-purple-600 to-sky-400 px-[20px] py-[16px] text-[18px] font-[600] text-neutral-100`}
     >
       {text}
     </button>
--- widgets/Header.tsx
+++ widgets/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
     <div className="bg-Gray5 sticky top-0 px-[200px] py-[20px]">
       <div className="flex justify-between">
         <Image src={logo} alt="logo" width={133} height={24} />
-        <GradientSkyblueBtn width={"128px"} text={"로그인"} />
+        <GradientSkyblueBtn width={"128"} text={"로그인"} />
       </div>
     </div>
   );
