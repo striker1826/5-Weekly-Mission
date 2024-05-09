2c0cf756f6efb1b91e2583f5b1abb4d8c7743828
striker1826 <alstjq123579@daum.net>
2024-05-10 08:59:03 +0900

    Home Page Header 부분 Tablet 반응형 구현

--- widgets/home/Header.tsx
+++ widgets/home/Header.tsx
@@ -3,10 +3,17 @@ import React from "react";
 
 import logo from "@/public/img/logo.svg";
 import GradientSkyblueBtn from "@/components/GradientSkyblueBtn";
+import { cn } from "@/utils/tailwind";
 
 const Header = () => {
   return (
-    <div className="sticky top-0 bg-Gray5 px-[200px] py-[20px]">
+    <div
+      className={cn(
+        "sticky top-0 bg-Gray5 py-[20px]",
+        "tablet:px-[32px]",
+        "web:px-[200px]",
+      )}
+    >
       <div className="flex justify-between">
         <Image src={logo} alt="logo" width={133} height={24} />
         <GradientSkyblueBtn width={128} text={"로그인"} />
