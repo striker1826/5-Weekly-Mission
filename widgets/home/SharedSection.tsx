5a8990b65bce12b41ac96617c51c3a104e1b870e
striker1826 <alstjq123579@daum.net>
2024-05-10 09:14:53 +0900

    Home Page Shared Section Tablet 반응형 구현

--- widgets/home/SharedSection.tsx
+++ widgets/home/SharedSection.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
 import React from "react";
 import shared_section_img from "@/public/img/home/shared_section.png";
-import shared_section_popup from "@/public/img/home/shared_section_popup.png";
+
+import { cn } from "@/utils/tailwind";
 
 const SharedSection = () => {
   return (
-    <section className="my-[50px] flex items-center justify-center gap-[157px]">
+    <section
+      className={cn(
+        "mt-[100px] flex items-center justify-center",
+        "tablet:gap-[51px]",
+        "web:gap-[157px]",
+      )}
+    >
       <div className="flex w-[290px] flex-col gap-[10px] pr-[15px]">
         <p className="text-[48px] font-[700] leading-[-0.3px] text-[#000]">
-          저장한 링크를{" "}
+          저장한 링크를
           <span className="bg-gradient-to-r from-indigo-600 via-indigo-400 bg-clip-text text-transparent">
             공유
           </span>
@@ -19,22 +26,17 @@ const SharedSection = () => {
           쉽고 빠르게 링크를 공유해 보세요.
         </p>
       </div>
-      <div className="relative">
-        <Image
-          className="rounded-[25px] brightness-50"
-          src={shared_section_img}
-          alt="Shared Section img"
-          width={550}
-          height={450}
-        />
-        <Image
-          className="absolute left-[50px] top-[50px] translate-x-[0%] translate-y-[25%]"
-          src={shared_section_popup}
-          alt="Shared Section Popup img"
-          width={416}
-          height={271}
-        />
-      </div>
+      <Image
+        className={cn(
+          "rounded-[25px]",
+          "tablet:w-[385px] tablet:h-[315px]",
+          "web:w-[550px] web:h-[450px]",
+        )}
+        src={shared_section_img}
+        alt="Shared Section img"
+        width={550}
+        height={450}
+      />
     </section>
   );
 };
